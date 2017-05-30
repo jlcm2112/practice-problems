68351e8ec43f04a5acc3e04bc4e0061bcea9fe40
Jamie Milhoan <jlmilhoan@gmail.com>
2017-05-30 14:06:03 -0400

    d

--- SortingArrayList.java
+++ SortingArrayList.java
@@ -7,19 +7,21 @@ public class SortingArrayList {
 	public static void main(String[] args) {
 		Random generator = new Random();
 		ArrayList foo = new ArrayList();
-		
-		for(int i=0; i<20; i++){
+
+		for (int i = 0; i < 20; i++) {
 			foo.add(10 + generator.nextInt(90));
 		}
 		System.out.println("ArrayList before: " + foo);
-		
-		//Integer largest;
-	 	//for(int i=0; i<foo.size(); i++){	
-	 		Collections.sort(foo);
-	 		Collections.reverse(foo);
-	 //	}
+
+		sort(foo);
 		System.out.println("ArrayList after: " + foo);
 
+	}
+
+	public static ArrayList sort(ArrayList foo) {
+		Collections.sort(foo);
+		Collections.reverse(foo);
+		return foo;
 	}
 
 }
